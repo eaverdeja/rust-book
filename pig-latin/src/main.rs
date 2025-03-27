96c5ade6d7ccb6f5e0f8e5362239ae10d7d781e8
eaverdeja <eaverdeja@gmail.com>
2025-03-27 19:05:42 -0300

    super pig latin

--- pig-latin/src/main.rs
+++ pig-latin/src/main.rs
@@ -4,34 +4,35 @@
 // a vowel have hay added to the end instead (apple becomes apple-hay).
 // Keep in mind the details about UTF-8 encoding!
 
-use std::io;
+use std::io::{self, Write};
 
-fn main() {
+fn to_pig_latin(word: &str) -> String {
+    match word.chars().next().expect("Cannot convert empty string!") {
+        'a' | 'e' | 'i' | 'o' | 'u' => {
+            format!("{}-hay", word)
+        }
+        _ => {
+            let (first_letter, rest) = word.split_at(1);
+            format!("{}-{}ay", rest, first_letter)
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
     loop {
         println!("Pig-latin translator. Input your text:");
+        io::stdout().flush()?;
 
         let mut s = String::new();
-        io::stdin().read_line(&mut s).expect("Failed to read line!");
+        io::stdin().read_line(&mut s)?;
 
-        let words: Vec<&str> = s.trim().split_whitespace().collect();
-
-        if words.is_empty() {
+        let input = s.trim();
+        if input.is_empty() {
             println!("Please enter a valid string!");
             continue;
         }
 
-        for word in words {
-            let res = match word.chars().next().expect("Cannot convert empty string!") {
-                'a' | 'e' | 'i' | 'o' | 'u' => {
-                    format!("{}-hay", word)
-                }
-                _ => {
-                    let (first_letter, rest) = word.split_at(1);
-                    format!("{}-{}ay", rest, first_letter)
-                }
-            };
-            print!("{res} ");
-        }
-        println!("");
+        let pig_latin_words: Vec<String> = input.split_whitespace().map(to_pig_latin).collect();
+        println!("{}", pig_latin_words.join(" "));
     }
 }
